1d0941389f4f3d3177cfd4d4163f331121e8867c
23-Pravallika <learngain23@gmail.com>
2024-02-02 13:02:32 +0530

    AutoPush

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,23 +1,23 @@
 def lintchecks(COMPONENT){
     sh '''      
-            echo Lint Checks 
+            echo Lint Checks for ${COMPONENT}
             echo installing jslint
             # npm install jslint
             # node_modules/jslint/bin/jslint.js server.js
-            echo performing lint checks 
-            echo performing lint checks completed 
+            echo performing lint checks for ${COMPONENT}
+            echo performing lint checks completed ${COMPONENT} 
     '''
 }
 
 //Call is the default function which will be called when you call the fileName
-def call(COMPONENT){
+def call(){
     pipeline {
         agent any 
         stages {
             stage('Lint Checks') {
                 steps {
                     script{
-                        lintchecks('COMPONENT')
+                        lintchecks()
                     }
                 }
             }
