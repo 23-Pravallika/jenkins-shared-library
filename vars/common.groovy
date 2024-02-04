eaf53ea1eed7f1717facae4caacb1da458d2d953
23-Pravallika <learngain23@gmail.com>
2024-02-04 20:24:40 +0530

    AutoPush

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,4 +1,4 @@
-def sonarChecks() {
+def sonarChecks(){
     //sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000 ${ARGS} -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
     //sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh" 
     //sh "bash -x quality-gate.sh ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}"
@@ -7,7 +7,7 @@ def sonarChecks() {
 
 }
 
-def test() {
+def test(){
     pipeline {
         agent any
         stages {
