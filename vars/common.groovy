af987111aa0327faa4763f60b0d8aff6c0d6ef35
23-Pravallika <learngain23@gmail.com>
2024-02-04 20:39:40 +0530

    AutoPush

--- vars/common.groovy
+++ vars/common.groovy
@@ -11,30 +11,26 @@ def test(){
     pipeline {
         agent any
         stages {
-            stage('testing') {
-                parallel {
-                    stage('Unit testing') {
-                        steps {
-                            //npm test or mvn test or py test 
-                            sh "echo Unit Testing Started"
-                            sh "echo Unit Testing Completed"                       
-                        }
-                    }
-                    stage('Integration testing') {
-                        steps {
-                            //npm verify or mvn verify or pr verify
-                            sh "echo Integration Testing Started"
-                            sh "echo Integration Testing Completed"
-                        }
-                    }
-                    stage('Functional testing') {
-                        steps {
-                            sh "echo Functional Testing Started"
-                            sh "echo Functional Testing Started"
-                        }
-                    }
+            stage('Unit testing') {
+                steps {
+                    //npm test or mvn test or py test 
+                    sh "echo Unit Testing Started"
+                    sh "echo Unit Testing Completed"                       
                 }
             }
-        }
+            stage('Integration testing') {
+                steps {
+                    //npm verify or mvn verify or pr verify
+                    sh "echo Integration Testing Started"
+                    sh "echo Integration Testing Completed"
+                }
+            }
+            stage('Functional testing') {
+                steps {
+                    sh "echo Functional Testing Started"
+                    sh "echo Functional Testing Started"
+                }
+            }
+        }   
     }
 }
